9caa7f292e477e8423e3f683c05bade12309af06
Max <max@maxf.net>
2023-05-14 21:18:00 -0400

    added

--- git/diff/openssl/textconv.sh
+++ git/diff/openssl/textconv.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 . .git/git-setenv-openssl-secrets.sh
 
-openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -base64 -k $GIT_FILTER_OPENSSL_PASSWORD -in "$1" 2> /dev/null || cat "$1"
+if [ "$GIT_FILTER_OPENSSL_DEBUG" = "true" ]; then
+	echo $0 $* >&2
+	set -x
+fi
+
+openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -base64 -salt -S $GIT_FILTER_OPENSSL_SALT -k $GIT_FILTER_OPENSSL_PASSWORD -in "$1" 2> /dev/null || cat "$1"
