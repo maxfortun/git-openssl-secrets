21bcd9138fa473507adba1ede08c2919b7af01f4
Max <max@maxf.net>
2024-02-21 10:48:48 -0500

    updated

---
+++ git-setenv-openssl-secrets-aws.sh
@@ -0,0 +1,15 @@
+cache=${BASH_SOURCE[0]}.cache
+
+if [ ! -f $cache ]; then
+	AWS=aws
+	AWS_SSM_PREFIX=/git-secrets/openssl
+
+	cat <<_EOT_ > $cache
+export GIT_FILTER_OPENSSL_SALT=${GIT_FILTER_OPENSSL_SALT:-$($AWS --region $AWS_REGION --output text ssm get-parameter --with-decryption --name "$AWS_SSM_PREFIX-salt" --query 'Parameter.Value')}
+export GIT_FILTER_OPENSSL_PASSWORD=${GIT_FILTER_OPENSSL_PASSWORD:-$($AWS --region $AWS_REGION --output text ssm get-parameter --with-decryption --name "$AWS_SSM_PREFIX-password" --query 'Parameter.Value')}
+_EOT_
+
+fi
+
+. $cache
+
--- git-setenv-openssl-secrets-vault.sh
+++ git-setenv-openssl-secrets-vault.sh
@@ -1,12 +1,21 @@
-. vault-functions.sh
+cache=${BASH_SOURCE[0]}.cache
 
-VAULT_NAMESPACE=parentns/childns
+if [ ! -f $cache ]; then
+	VAULT_NAMESPACE=parentns/childns
+	VAULT_ROLE=user
+	VAULT_PREFIX=git-secrets/openssl
 
-( [ -z "$GIT_FILTER_OPENSSL_PASSWORD" ] || [ -z "$GIT_FILTER_OPENSSL_SALT" ] ) && IS_VAULT_REQUIRED=true
-[ -z "$IS_VAULT_REQUIRED" ] || vault-login-aws $VAULT_NAMESPACE user
+	( [ -z "$GIT_FILTER_OPENSSL_PASSWORD" ] || [ -z "$GIT_FILTER_OPENSSL_SALT" ] ) && IS_VAULT_REQUIRED=true
+	if [ -n "$IS_VAULT_REQUIRED" ]; then
+		export VAULT_TOKEN=$(vault login -namespace=$VAULT_NAMESPACE -token-only -method=aws region=${AWS_STS_REGION:-$AWS_REGION} -format=table header_value=$VAULT_HOST role=$VAULT_ROLE)
+	fi
 
-VAULT_PREFIX=git-secrets/openssl
 
-export GIT_FILTER_OPENSSL_PASSWORD=${GIT_FILTER_OPENSSL_PASSWORD:-$(vault-read $VAULT_PREFIX-password)}
-export GIT_FILTER_OPENSSL_SALT=${GIT_FILTER_OPENSSL_SALT:-$(vault-read $VAULT_PREFIX-salt)}
+	cat <<_EOT_ > $cache
+export GIT_FILTER_OPENSSL_PASSWORD=${GIT_FILTER_OPENSSL_PASSWORD:-$(vault read -ns=$VAULT_NAMESPACE -field=value -format=table secret/$VAULT_PREFIX-password)}
+export GIT_FILTER_OPENSSL_SALT=${GIT_FILTER_OPENSSL_SALT:-$(vault read -ns=$VAULT_NAMESPACE -field=value -format=table secret/$VAULT_PREFIX-salt)}
+_EOT_
 
+fi
+
+. $cache
