fe8cac73be647a60829df64856d8b51d338b086c
Max <max@maxf.net>
2025-07-06 10:56:59 -0400

    updated

--- git/filter/openssl/smudge.sh
+++ git/filter/openssl/smudge.sh
@@ -8,21 +8,17 @@ fi
 
 [ ! -f .secrets/git-setenv-openssl-secrets.sh ] || . .secrets/git-setenv-openssl-secrets.sh
 
-TMP_FILE="/tmp/$(basename $0).$LOGNAME.$$"
-cat > "$TMP_FILE"
-
-if cat "$TMP_FILE" | base64 -d > "$TMP_FILE.decoded" 2>/dev/null; then
+if cat "$file" | base64 -d > "$file.decoded" 2>/dev/null; then
 
 	# Legacy support.
-	if ! head -1 "$TMP_FILE.decoded" | cut -b1-8 | grep -q ^Salted__; then
+	if ! head -1 "$file.decoded" | cut -b1-8 | grep -q ^Salted__; then
     	SALT_PARAMS="-S $GIT_FILTER_OPENSSL_SALT"
 	fi
 
-	openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 $SALT_PARAMS -k $GIT_FILTER_OPENSSL_PASSWORD -in "$TMP_FILE.decoded" 2> /dev/null || cat "$TMP_FILE"
+	openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 $SALT_PARAMS -k $GIT_FILTER_OPENSSL_PASSWORD -in "$file.decoded" 2> /dev/null || cat "$file"
 else
-	cat "$TMP_FILE"
+	cat "$file"
 fi
-rm "$TMP_FILE"*
 
 trackingDir=".git/filter/openssl/tracking"
 trackingFile="$trackingDir/$file"
